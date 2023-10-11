7bfd5ccc59d51f1a1b45cdc074c3548d448f9434
arda oe <ardarda@gmail.com>
2023-10-11 04:15:20 +0300

    sadece 6 basamak eksik

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,50 +31,211 @@
             button1 = new Button();
             textBox1 = new TextBox();
             button2 = new Button();
-            listView1 = new ListView();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            listBox2 = new ListBox();
+            button4 = new Button();
+            textBox2 = new TextBox();
+            button5 = new Button();
+            checkBox1 = new CheckBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button6 = new Button();
+            button7 = new Button();
+            textBox7 = new TextBox();
+            outputTextBox = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(533, 12);
+            button1.Location = new Point(533, 107);
             button1.Name = "button1";
-            button1.Size = new Size(137, 23);
+            button1.Size = new Size(255, 23);
             button1.TabIndex = 0;
-            button1.Text = "Pick Rhubarb JSON";
+            button1.Text = "Pick/Change  Rhubarb JSON";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 13);
+            textBox1.Location = new Point(12, 108);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(515, 23);
             textBox1.TabIndex = 1;
             // 
             // button2
             // 
-            button2.Location = new Point(533, 41);
+            button2.Location = new Point(533, 414);
             button2.Name = "button2";
-            button2.Size = new Size(137, 23);
+            button2.Size = new Size(255, 23);
             button2.TabIndex = 2;
-            button2.Text = "Parse JSON";
+            button2.Text = "4. Parse JSON";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
-            // listView1
+            // listBox1
             // 
-            listView1.Location = new Point(12, 51);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(515, 363);
-            listView1.TabIndex = 3;
-            listView1.UseCompatibleStateImageBehavior = false;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 149);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(515, 289);
+            listBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(533, 139);
+            button3.Name = "button3";
+            button3.Size = new Size(137, 23);
+            button3.TabIndex = 4;
+            button3.Text = "3.Pick Image Folder";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.HorizontalScrollbar = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(533, 168);
+            listBox2.Name = "listBox2";
+            listBox2.ScrollAlwaysVisible = true;
+            listBox2.Size = new Size(255, 184);
+            listBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(533, 443);
+            button4.Name = "button4";
+            button4.Size = new Size(255, 23);
+            button4.TabIndex = 6;
+            button4.Text = "5. save ffconcat file";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 472);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(515, 23);
+            textBox2.TabIndex = 7;
+            textBox2.Text = "create concat file";
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button5
+            // 
+            button5.Enabled = false;
+            button5.Location = new Point(533, 472);
+            button5.Name = "button5";
+            button5.Size = new Size(255, 23);
+            button5.TabIndex = 8;
+            button5.Text = "6. Convert to mp4";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Checked = true;
+            checkBox1.CheckState = CheckState.Checked;
+            checkBox1.Location = new Point(533, 358);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(97, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Use full paths";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(688, 139);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 10;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 501);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(141, 23);
+            textBox4.TabIndex = 11;
+            textBox4.Text = "ffmpeg -f concat -safe 0 -i <fct> -c:a aac -c:v libx264 -pix_fmt yuv420p -s <res> -r 25 <out>";
+            textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(159, 501);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(368, 23);
+            textBox5.TabIndex = 12;
+            textBox5.Text = "rhubarb.exe <wav> --machineReadable -f json -r phonetic -o <json>";
+            textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(12, 20);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(515, 23);
+            textBox6.TabIndex = 14;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(533, 20);
+            button6.Name = "button6";
+            button6.Size = new Size(255, 23);
+            button6.TabIndex = 13;
+            button6.Text = "1. Pick Wav File To Prepare";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(533, 49);
+            button7.Name = "button7";
+            button7.Size = new Size(255, 23);
+            button7.TabIndex = 15;
+            button7.Text = "2. Create Rhubarb JSON";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(12, 50);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(515, 23);
+            textBox7.TabIndex = 16;
+            textBox7.Text = "Pick a wav first";
+            // 
+            // outputTextBox
+            // 
+            outputTextBox.Location = new Point(12, 79);
+            outputTextBox.Name = "outputTextBox";
+            outputTextBox.Size = new Size(776, 23);
+            outputTextBox.TabIndex = 17;
+            outputTextBox.Text = "...";
+            outputTextBox.TextChanged += outputTextBox_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(listView1);
+            ClientSize = new Size(800, 536);
+            Controls.Add(outputTextBox);
+            Controls.Add(textBox7);
+            Controls.Add(button7);
+            Controls.Add(textBox6);
+            Controls.Add(button6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(checkBox1);
+            Controls.Add(button5);
+            Controls.Add(textBox2);
+            Controls.Add(button4);
+            Controls.Add(listBox2);
+            Controls.Add(button3);
+            Controls.Add(listBox1);
             Controls.Add(button2);
             Controls.Add(textBox1);
             Controls.Add(button1);
@@ -90,6 +251,20 @@
         private Button button1;
         private TextBox textBox1;
         private Button button2;
-        private ListView listView1;
+        private ListBox listBox1;
+        private Button button3;
+        private ListBox listBox2;
+        private Button button4;
+        private TextBox textBox2;
+        private Button button5;
+        private CheckBox checkBox1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button6;
+        private Button button7;
+        private TextBox textBox7;
+        private TextBox outputTextBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,13 @@
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace GenRhubarb
 {
@@ -17,6 +21,17 @@ namespace GenRhubarb
             string jsonFilePath = textBox1.Text;
             string jsonString = File.ReadAllText(jsonFilePath);
 
+            string[] filenames;
+
+            if (checkBox1.Checked)
+            {
+                filenames = ExtractFullPathFromListBox(listBox2);
+            }
+            else
+            {
+                filenames = ExtractFilenamesFromListBox(listBox2);
+            }
+
             // Deserialize the JSON into your C# object
             var options = new JsonSerializerOptions
             {
@@ -27,17 +42,70 @@ namespace GenRhubarb
             // Access the mouthCues data
             List<MouthCue> mouthCues = jsonRoot.MouthCues;
 
-
-            listView1.Items.Clear();
+            listBox1.Items.Add("ffconcat version 1.0");
 
             foreach (MouthCue mouthCue in mouthCues)
             {
-                ListViewItem item = new ListViewItem(mouthCue.Start.ToString()); // Add the Start value
-                item.SubItems.Add(mouthCue.End.ToString()); // Add the End value
-                item.SubItems.Add(mouthCue.Value); // Add the Value
-                listView1.Items.Add(item); // Add the item to the ListView
+                string correspondingFilename = "NA";
+                char firstChar = mouthCue.Value[0];
+                int charCode = (int)firstChar;
+                int index = charCode - 64;
+                if (index >= 10) index = 8;
+
+                if (index >= 0)
+                {
+                    correspondingFilename = filenames[index];
+
+                }
+
+                double diff = mouthCue.End - mouthCue.Start;
+                listBox1.Items.Add("file " + correspondingFilename);
+                listBox1.Items.Add("duration " + diff.ToString("F4"));// Add the item to the ListView
+            }
+
+
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(string path, StringBuilder shortPath, int shortPathLength);
+        public static string GetShortFilename(string longFilename)
+        {
+            int bufferSize = 260; // MAX_PATH
+            StringBuilder shortPath = new StringBuilder(bufferSize);
+
+            int result = GetShortPathName(longFilename, shortPath, bufferSize);
+
+            if (result == 0)
+            {
+                // An error occurred
+                throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return shortPath.ToString();
+        }
+
+
+
+
+        static void SaveListBoxItemsToFile(ListBox listBox, string filePath)
+        {
+            try
+            {
+                string[] items = new string[listBox.Items.Count];
+                for (int i = 0; i < listBox.Items.Count; i++)
+                {
+                    items[i] = listBox.Items[i].ToString();
+                }
+
+                File.WriteAllLines(filePath, items);
+                Console.WriteLine("ListBox items have been saved to " + filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error saving ListBox items: " + ex.Message);
             }
         }
+
 
         public Form1()
         {
@@ -83,6 +151,262 @@ namespace GenRhubarb
         private void button2_Click(object sender, EventArgs e)
         {
             readjson();
+        }
+
+        static string[] ExtractFilenamesFromListBox(ListBox listBox)
+        {
+            List<string> filenames = new List<string>();
+
+            foreach (var item in listBox.Items)
+            {
+                if (item is string fullPath)
+                {
+                    string filename = System.IO.Path.GetFileName(fullPath);
+                    filenames.Add(filename);
+                }
+            }
+
+            return filenames.ToArray();
+        }
+
+        static string[] ExtractFullPathFromListBox(ListBox listBox)
+        {
+            List<string> filenames = new List<string>();
+
+            foreach (var item in listBox.Items)
+            {
+                if (item is string fullPath)
+                {
+                    //string filename = System.IO.Path.GetFileName(fullPath);
+                    filenames.Add(GetShortFilename(fullPath).Replace('\\', '/'));
+                }
+            }
+
+            return filenames.ToArray();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Create an instance of OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the initial directory (optional)
+            //openFileDialog.InitialDirectory = "C:\\";
+
+            // Filter the types of files that can be opened
+            openFileDialog.Filter = "Mouth Shape Image A (*A.png)|*A.png|All Files (*.*)|*.*";
+
+            // Filter index for the default selection (optional)
+            openFileDialog.FilterIndex = 1;
+
+            // Allow multiple file selections (optional)
+            openFileDialog.Multiselect = false;
+
+            // Set the title of the dialog
+            openFileDialog.Title = "Open a File";
+
+            // Show the dialog and check if the user clicked OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file path
+                string Afile = openFileDialog.FileName;
+
+                using (Image image = Image.FromFile(Afile))
+                {
+                    int width = image.Width;
+                    int height = image.Height;
+                    textBox3.Text = width + "x" + height;
+                }
+
+                string folderPath = Path.GetDirectoryName(Afile);
+                char[] validEndings = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'X' };
+
+                if (Directory.Exists(folderPath))
+                {
+                    string[] pngFiles = Directory.GetFiles(folderPath, "*.png");
+
+                    // Filter the files that meet the criteria
+                    var matchingFiles = pngFiles.Where(filePath =>
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(filePath);
+                        if (!string.IsNullOrEmpty(fileName) && fileName.Length >= 1)
+                        {
+                            char lastChar = char.ToUpper(fileName[^1]); // Get the last character and convert it to uppercase
+                            return validEndings.Contains(lastChar);
+                        }
+                        return false;
+                    });
+
+                    // Output the matching file paths
+                    //Console.WriteLine("Matching PNG files:");
+                    int total = 0;
+                    listBox2.Items.Clear();
+
+                    foreach (string filePath in matchingFiles)
+                    {
+                        total++;
+                        listBox2.Items.Add(filePath);
+                    }
+                    if (total < validEndings.Length)
+                    {
+                        button2.Text = "MISSING ENTRIES!!!";
+                    }
+                    else
+                    {
+                        button2.Text = "Parse Json Now!";
+
+                    }
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string jsonFilePath = textBox1.Text;
+            string newPath = Path.ChangeExtension(jsonFilePath, "fct");
+            string newOutPath = Path.ChangeExtension(jsonFilePath, "mp4");
+            newOutPath = System.IO.Path.GetFileName(newOutPath);
+            SaveListBoxItemsToFile(listBox1, newPath);
+            newPath = System.IO.Path.GetFileName(newPath);
+            textBox2.Text = textBox4.Text.Replace("<res>", textBox3.Text).Replace("<fct>", newPath).Replace("<out>", newOutPath);
+            button5.Enabled = true;
+
+
+        }
+
+        void updateExec()
+        {
+            string jsonFilePath = textBox1.Text;
+            string newPath = Path.ChangeExtension(jsonFilePath, "fct");
+            string newOutPath = Path.ChangeExtension(jsonFilePath, "mp4");
+            newOutPath = System.IO.Path.GetFileName(newOutPath);
+            newPath = System.IO.Path.GetFileName(newPath);
+            textBox2.Text = textBox4.Text.Replace("<res>", textBox3.Text).Replace("<fct>", newPath).Replace("<out>", newOutPath);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            updateExec();
+        }
+        static async Task ExecuteCommandAsync(string command, TextBox outputTextBox)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c " + command;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                // Create a synchronization context to run updates on the UI thread
+                SynchronizationContext context = SynchronizationContext.Current;
+
+                process.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(e.Data))
+                    {
+                        context.Post(_ =>
+                        {
+                            outputTextBox.Text = e.Data;
+                        }, null);
+                    }
+                };
+
+                process.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(e.Data))
+                    {
+                        context.Post(_ =>
+                        {
+                            outputTextBox.Text = e.Data;
+
+                        }, null);
+                    }
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                // Use Task.Run to execute the command asynchronously
+                await Task.Run(() => process.WaitForExit());
+            }
+        }
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            // Create an instance of OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set the initial directory (optional)
+            //openFileDialog.InitialDirectory = "C:\\";
+
+            // Filter the types of files that can be opened
+            openFileDialog.Filter = "Wav Files (*.wav)|*.wav|Ogg files (*.ogg)|*.ogg|All Files (*.*)|*.*";
+
+            // Filter index for the default selection (optional)
+            openFileDialog.FilterIndex = 1;
+
+            // Allow multiple file selections (optional)
+            openFileDialog.Multiselect = false;
+
+            // Set the title of the dialog
+            openFileDialog.Title = "Open a File";
+
+            // Show the dialog and check if the user clicked OK
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file path
+                string oFilePath = openFileDialog.FileName;
+                textBox6.Text = oFilePath;
+
+                string jsonFilePath = textBox6.Text;
+                string newPath = Path.ChangeExtension(jsonFilePath, "json");
+
+                jsonFilePath = System.IO.Path.GetFileName(jsonFilePath);
+                newPath = System.IO.Path.GetFileName(newPath);
+                textBox7.Text = textBox5.Text.Replace("<wav>", jsonFilePath).Replace("<json>", newPath);
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            runRhubarb();
+
+
+        }
+        private async void runRhubarb()
+        {
+            string command = textBox7.Text;
+
+            await ExecuteCommandAsync(command, outputTextBox);
+        }
+
+        private void outputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (outputTextBox.Text.Contains("uccess"))
+            {
+                string jsonFilePath = textBox6.Text;
+                string newPath = Path.ChangeExtension(jsonFilePath, "json");
+                newPath = System.IO.Path.GetFileName(newPath);
+                if (File.Exists(newPath))
+                {
+                    textBox1.Text = Path.ChangeExtension(jsonFilePath, "json");
+
+                }
+            }
         }
     }
 
